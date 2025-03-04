d082b4a4cf36fd71f6236c752e7d917f5fe425cc
Arseniy Mesherakov <user@nights.my.domain>
2025-03-04 21:41:08 +0300

    added movement

--- source/engine/engine.d
+++ source/engine/engine.d
@@ -3,6 +3,7 @@ module engine.engine;
 import std.string;
 import std.stdio;
 import std.conv;
+import std.math;
 
 import multimedia.playback;
 import player.controls;
@@ -13,6 +14,11 @@ import ui.widgets;
 import readers.control_parse;
 
 import raylib;
+
+struct KeyAction {
+    int key;
+    void delegate() action;
+}
 
 class Engine {
     char[4] buttonLayout; //if someone here would need it, but not outside
@@ -29,15 +35,15 @@ class Engine {
     }
 
     void engineConfigurator() {
-        scene = new Scene(camera);
+        //models
+        Model playerModel = LoadModel("res/mc.glb");
+        player = new Player("quantumde1", 120, 0, 1, playerModel, Vector3(0, 0, 0), Vector3(1, 1, 1));
+        scene = new Scene(camera, player.coordinates);
         scene.camera.position = Vector3(0.0, 10.0, 15.0);
         scene.camera.target = Vector3(0.0, 4.0, 0.0);
         scene.camera.up = Vector3(0.0, 1.0, 0.0);
         scene.camera.projection = CameraProjection.CAMERA_PERSPECTIVE;
         scene.camera.fovy = 45.0f;
-        //models
-        Model playerModel = LoadModel("res/mc.glb");
-        player = new Player("quantumde1", 120, 0, 1, playerModel, Vector3(0, 0, 0), Vector3(1, 1, 1));
     }
 
     void engineStarter() {
@@ -51,28 +57,43 @@ class Engine {
         immutable int backward = controls.backward.to!int;
         immutable int left = controls.left.to!int;
         immutable int right = controls.right.to!int;
+
+        KeyAction[] keyActionsPlayer = [
+            {forward, () { 
+                player.coordinates.z -= 0.2;
+                scene.camera.position.z -= 0.2;
+                scene.camera.target.z -= 0.2;
+            }},
+            {backward, () { 
+                player.coordinates.z += 0.2;
+                scene.camera.position.z += 0.2;
+                scene.camera.target.z += 0.2;
+            }},
+            {left, () { 
+                player.coordinates.x -= 0.2;
+                scene.camera.position.x -= 0.2;
+                scene.camera.target.x -= 0.2;
+            }},
+            {right, () { 
+                player.coordinates.x += 0.2;
+                scene.camera.position.x += 0.2;
+                scene.camera.target.x += 0.2;
+            }},
+        ];
         while (WindowShouldClose() == false) {
             BeginDrawing();
             ClearBackground(Colors.WHITE);
+            DrawText(player.coordinates.z.to!string.toStringz, 40, 40, 30, Colors.BLACK);
             BeginMode3D(scene.camera);
             DrawModel(player.model, player.coordinates, player.scale.x, Colors.WHITE);
-            int pressedButton = GetKeyPressed();
-            switch (pressedButton) {
-                case forward:
-                    writeln("Pressed forward.");
-                    break;
-                case backward:
-                    writeln("Pressed backward.");
-                    break;
-                case left:
-                    writeln("Pressed left.");
-                    break;
-                case right:
-                    writeln("Pressed right.");
-                    break;
-                default:
-                    break;
+            DrawGrid(20, 3);
+            scene.rotateCamera(player.coordinates);
+            foreach (keyAction; keyActionsPlayer) {
+                if (IsKeyDown(keyAction.key)) {
+                    keyAction.action();
+                }
             }
+
             EndMode3D();
             EndDrawing();
         }
--- source/scene/loader.d
+++ source/scene/loader.d
@@ -1,6 +1,7 @@
 module scene.loader;
 
 import std.stdio;
+import std.math;
 
 import raylib;
 
@@ -8,8 +9,30 @@ import scene.objects;
 
 class Scene {
     Camera3D camera;
-    this(Camera3D camera) {
+    Vector3 playerPosition;
+
+    float cameraAngle = 90;
+    float rotationStep = 1;
+    float radius = 10;
+
+    this(Camera3D camera, Vector3 playerPosition) {
         this.camera = camera;
+        this.playerPosition = playerPosition;
+    }
+
+    void rotateCamera(Vector3 playerPosition) {
+        if (IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+            cameraAngle = fmod(cameraAngle + rotationStep, 360.0f);
+            if (cameraAngle < 0) cameraAngle += 360.0f;
+        }
+        if (IsKeyDown(KeyboardKey.KEY_LEFT)) {
+            cameraAngle = fmod(cameraAngle - rotationStep, 360.0f);
+            if (cameraAngle < 0) cameraAngle += 360.0f;
+        }
+
+        float cameraX = playerPosition.x + radius * cos(cameraAngle * std.math.PI / 180.0f);
+        float cameraZ = playerPosition.z + radius * sin(cameraAngle * std.math.PI / 180.0f);
+        camera.position = Vector3(cameraX, camera.position.y, cameraZ);
     }
 }
 
