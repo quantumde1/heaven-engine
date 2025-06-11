e1fd732aed2de9d722a2509a6ad9816ccf73ff63
quantumde1 <meserakovarsenij810@gmail.com>
2025-06-11 20:17:45 +0300

    idek

--- source/graphics/menus.c
+++ source/graphics/menus.c
@@ -1,6 +1,6 @@
 #include <raylib.h>
 
-void drawRectangleWithCenteredText(int x, int y, Vector2 size, char* text, Color colorRectangle, Color colorText) {
+void drawRectangleWithCenteredText(Font font, int x, int y, Vector2 size, char* text, Color colorRectangle, Color colorText) {
     DrawRectangle(x, y, size.x, size.y, colorRectangle);
     
     int fontSize = 20;
@@ -9,13 +9,13 @@ void drawRectangleWithCenteredText(int x, int y, Vector2 size, char* text, Color
     int textX = x + (size.x - textWidth) / 2;
     int textY = y + (size.y - fontSize) / 2;
     
-    DrawText(text, textX, textY, fontSize, colorText);
+    DrawTextEx(font, text, (Vector2){textX, textY}, fontSize, 1.0f, colorText);
 }
 
-void gameMenu() {
+void gameMenu(Font font) {
     char* menuItems[5] = {"Continue", "Save", "Load", "Backlog", "Exit"};
     int startY = 20;
     for (int i = 0; i < 5; i++) {
-        drawRectangleWithCenteredText(20, startY + i * 40, (Vector2){120, 60}, menuItems[i], SKYBLUE, WHITE);
+        drawRectangleWithCenteredText(font, 23, startY + i * 40, (Vector2){140, 40}, menuItems[i], (Color){20, 20, 20, 220}, WHITE);
     }
 }
